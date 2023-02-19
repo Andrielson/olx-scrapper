7428fadc6e729edc4394c51ebc3db27f473c9045
Andrielson Ferreira da Silva <andrielson@gmail.com>
2023-02-19 01:17:27 -0300

    feat: adjust house filters

--- src/check-ignored-house.ts
+++ src/check-ignored-house.ts
@@ -2,21 +2,40 @@ import { HouseRecord } from "./types";
 
 const IGNORED_NEIGHBORHOODS: string[] = [
   "Boa Vista",
-  "Parque Industrial",
   "Centro",
-  "Vila São Pedro",
-  "Residencial Ary Attab",
-  "Eldorado",
-  "Vila Clementina",
-  "Residencial Ana Célia",
-  "Jardim João Paulo II",
-  "Jardim Canaã",
-  "Fazenda Retiro (Zona Rural)",
   "Chácara Jockey Club (Zona Rural)",
+  "Eldorado",
+  "Fazenda Retiro (Zona Rural)",
+  "Jardim Canaã",
+  "Jardim Congonhas",
+  "Jardim João Paulo II",
+  "Jardim Nunes",
+  "Jardim Residencial Vetorasso",
+  "Jardim Suzana",
+  "Parque das Aroeiras",
+  "Parque Industrial",
+  "Parque Jaguaré",
+  "Parque Residencial Joaquim Nabuco",
+  "Parque Residencial Nature I",
+  "Parque Residencial Universo",
+  "Residencial Ana Célia",
+  "Residencial Ary Attab",
+  "Residencial Macedo Teles I",
+  "Residencial Nato Vetorasso",
+  "Setparque Avenida 2",
+  "Solo Sagrado I",
+  "Vila Anchieta",
+  "Vila Clementina",
+  "Vila Esplanada",
+  "Vila São Pedro",
+  "Vila Sinibaldi",
+  "Vila Novaes",
+  "Vila Moreira",
+  "Jardim Santa Rosa I",
 ];
 const MIN_BEDROOMS = 3;
 const MIN_RENT_AMOUNT = 1500;
-const MAX_RENT_AMOUNT = 4500;
+const MAX_RENT_AMOUNT = 3500;
 
 export function checkIgnoredHouse(house: HouseRecord) {
   if (!!house.bedrooms && house.bedrooms < MIN_BEDROOMS) return true;
